bb61ab492928e08c3713e4e06caf2ef5048f4330
analog-sombra <plexsido.kk@gmail.com>
2022-11-05 10:23:43 +0530

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:developer';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -35,7 +36,6 @@ class UserList extends StatefulWidget {
 class _UserListState extends State<UserList> {
   List users = [];
   void init() async {
-    log("goind");
     try {
       final String response =
           await rootBundle.loadString('assets/data/userdata.json');
@@ -51,6 +51,7 @@ class _UserListState extends State<UserList> {
   @override
   void initState() {
     super.initState();
+
     init();
   }
 
@@ -58,14 +59,395 @@ class _UserListState extends State<UserList> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("User List"),
+        backgroundColor: const Color(0xff10b981),
+        centerTitle: true,
+        title: const Text(
+          "USER LIST",
+          textScaleFactor: 1,
+        ),
       ),
       body: SafeArea(
           child: ListView.builder(
               itemCount: users.length,
-              itemBuilder: (context, item) {
-                return Text("great");
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: const Color(0xff10b981),
+                      child: Text(
+                        users[index]["name"][0],
+                        textScaleFactor: 1,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            fontSize: 25,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                    title: Text(
+                      "[${users[index]["id"]}] ${users[index]["name"]}",
+                      textScaleFactor: 1,
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black.withOpacity(0.85),
+                          fontWeight: FontWeight.w500),
+                    ),
+                    subtitle: Text(
+                      users[index]["atype"],
+                      textScaleFactor: 1,
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black.withOpacity(0.6),
+                          fontWeight: FontWeight.w400),
+                    ),
+                    trailing: SizedBox(
+                      width: 100,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          elevation: MaterialStateProperty.all(2),
+                          backgroundColor: MaterialStateProperty.all(
+                              const Color(0xff10b981)),
+                          shape: MaterialStateProperty.all(
+                            RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                          ),
+                          minimumSize: MaterialStateProperty.all(
+                              const Size.fromHeight(40)),
+                        ),
+                        onPressed: () {
+                          signAlert(context, users[index]["id"],
+                              users[index]["name"], users[index]["atype"]);
+                        },
+                        child: const Text(
+                          "Sign in",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
               })),
+    );
+  }
+
+  void signAlert(BuildContext context, String id, String name, String atype) {
+    TextEditingController age = TextEditingController();
+    String genderRadioBtnVal = "Male";
+
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+        child:
+            StatefulBuilder(builder: (BuildContext context, Function setState) {
+          return AlertDialog(
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(16.0))),
+            contentPadding: const EdgeInsets.all(5),
+            backgroundColor: Colors.white,
+            content: Container(
+              width: MediaQuery.of(context).size.width - 50,
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      const Spacer(),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: Icon(
+                          Icons.close,
+                          color: Colors.black.withOpacity(0.65),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    "Sign In",
+                    textScaleFactor: 1,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Color(0xff10b981),
+                        fontSize: 25,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 5),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: TextField(
+                        controller: age,
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: const Color(0xfff3f4f6),
+                          border: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          errorBorder: InputBorder.none,
+                          disabledBorder: InputBorder.none,
+                          hintText: "Age",
+                          hintStyle: TextStyle(
+                            color: Colors.black.withOpacity(0.45),
+                            fontSize: 20,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Radio<String>(
+                        value: "Male",
+                        hoverColor: const Color(0xff10b981),
+                        activeColor: const Color(0xff10b981),
+                        groupValue: genderRadioBtnVal,
+                        onChanged: (value) {
+                          setState(() {
+                            genderRadioBtnVal = value!;
+                          });
+                        },
+                      ),
+                      Text(
+                        "Male",
+                        textScaleFactor: 1,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black.withOpacity(0.65),
+                            fontWeight: FontWeight.w500),
+                      ),
+                      Radio<String>(
+                        value: "Female",
+                        hoverColor: const Color(0xff10b981),
+                        activeColor: const Color(0xff10b981),
+                        groupValue: genderRadioBtnVal,
+                        onChanged: (value) {
+                          setState(() {
+                            genderRadioBtnVal = value!;
+                          });
+                        },
+                      ),
+                      Text(
+                        "Female",
+                        textScaleFactor: 1,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black.withOpacity(0.65),
+                            fontWeight: FontWeight.w500),
+                      ),
+                      Radio<String>(
+                        value: "Other",
+                        hoverColor: const Color(0xff10b981),
+                        activeColor: const Color(0xff10b981),
+                        groupValue: genderRadioBtnVal,
+                        onChanged: (value) {
+                          setState(() {
+                            genderRadioBtnVal = value!;
+                          });
+                        },
+                      ),
+                      Text(
+                        "Other",
+                        textScaleFactor: 1,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black.withOpacity(0.65),
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      const Spacer(),
+                      SizedBox(
+                        width: 100,
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            elevation: MaterialStateProperty.all(2),
+                            backgroundColor: MaterialStateProperty.all(
+                                const Color(0xff10b981)),
+                            shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                            ),
+                            minimumSize: MaterialStateProperty.all(
+                                const Size.fromHeight(40)),
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => UserDetails(
+                                        id: int.parse(id),
+                                        name: name,
+                                        age: age.text,
+                                        atype: atype,
+                                        gender: genderRadioBtnVal)));
+                          },
+                          child: const Text(
+                            "Done",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
+
+class UserDetails extends StatefulWidget {
+  final int id;
+  final String name;
+  final String age;
+  final String gender;
+  final String atype;
+  const UserDetails(
+      {super.key,
+      required this.id,
+      required this.name,
+      required this.age,
+      required this.atype,
+      required this.gender});
+
+  @override
+  State<UserDetails> createState() => _UserDetailsState();
+}
+
+class _UserDetailsState extends State<UserDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xff10b981),
+        centerTitle: true,
+        title: Text(
+          widget.name,
+          textScaleFactor: 1,
+        ),
+      ),
+      body: SafeArea(
+          child: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 40,
+            ),
+            Center(
+              child: CircleAvatar(
+                radius: 50,
+                backgroundColor: const Color(0xff10b981),
+                child: Text(
+                  widget.name[0],
+                  textScaleFactor: 1,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                      fontSize: 55,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              widget.name,
+              textScaleFactor: 1,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.black.withOpacity(0.85),
+                  fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Text(
+              "Age : ${widget.age}",
+              textScaleFactor: 1,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black.withOpacity(0.65),
+                  fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Text(
+              "Gender : ${widget.gender}",
+              textScaleFactor: 1,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black.withOpacity(0.65),
+                  fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+              width: 120,
+            
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  elevation: MaterialStateProperty.all(2),
+                  backgroundColor:
+                      MaterialStateProperty.all(const Color(0xffef4444)),
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                  minimumSize:
+                      MaterialStateProperty.all(const Size.fromHeight(40)),
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text(
+                  "Sign Out",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ),
+            )
+          ],
+        ),
+      )),
     );
   }
 }
